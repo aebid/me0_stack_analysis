47376f30b2e0a089f96a9338ceaada42fedaeb44
aebid <aebid@tamu.edu>
2023-12-04 10:54:33 +0100

    Updated variable names

--- rechit_analysis/awk_cluster_analyzer.py
+++ rechit_analysis/awk_cluster_analyzer.py
@@ -6,9 +6,9 @@ import matplotlib.cm as cm
 import os
 from scipy.stats import norm
 
-plot_prefix = "plots/awk_cluster/"
+plot_prefix = "plots/run386/"
 
-fname = "../input_files/rechit/awk_cluster_tree.root"
+fname = "../awk_cluster_run386.root"
 
 #Data looks like
 #nclusterChamber = 7
@@ -51,7 +51,12 @@ events = tree.arrays()
 
 ch_list = [0,1,2,3]
 eta_list = [1,2,3,4]
-n_strip_bins = 3 #3 VFATs
+n_strip_bins = int(128*3 / 4) #n bins per eta partition
+n_stips_per_bin = 128*3/n_strip_bins
+
+
+if not os.path.exists(plot_prefix):
+    os.makedirs(plot_prefix)
 
 BX_options = [[events.clusterBX_first, "First_BX"], [events.clusterBX_second, "Second_BX"], [events.clusterBX_last, "Last_BX"], [events.clusterBX_avg, "Average_BX"], [events.clusterBX_avg, "Average_BX_round"]]
 
@@ -59,6 +64,7 @@ one_hit_per_chamber = (ak.sum(events.clusterChamber == 0, axis=1) == 1) & (ak.su
 print("one hit test ", one_hit_per_chamber)
 for bx, name in BX_options:
     print(name)
+    print(bx)
     strips = events.clusterStrip
 
     fig_occupancy, axs_occupancy = plt.subplots(4, 4, figsize=(20,20))
@@ -69,22 +75,11 @@ for bx, name in BX_options:
 
     fig_timeres_hist, axs_timeres_hist = plt.subplots(4, figsize=(20,20))
 
-    fig_BX_eta3_strips100_150, axs_BX_eta3_strips100_150 = plt.subplots(4, figsize=(20,20))
+    #Figure for the time resolution split by eta partitions
+    fig_timeres_hist_by_eta, axs_timeres_hist_by_eta = plt.subplots(4, 4, figsize=(20,20))
+
 
     for ch_num in ch_list:
-        eta3_strips100_150_mask = (events.clusterChamber == ch_num) & (events.clusterEta == 3) & (strips >= 100) & (strips <= 150)
-        if name == "Average_BX_round":
-            x = np.round(ak.to_numpy(ak.flatten(bx[eta3_strips100_150_mask])), 0)
-        else:
-            x = ak.to_numpy(ak.flatten(bx[eta3_strips100_150_mask]))
-
-        tmp_eta3_strips100_150_hist = axs_BX_eta3_strips100_150[ch_num].hist(x, bins=7, range=(0.0,7.0))
-        axs_BX_eta3_strips100_150[ch_num].set_xlabel(name)
-        axs_BX_eta3_strips100_150[ch_num].set_ylabel("Entries")
-        axs_BX_eta3_strips100_150[ch_num].set_title("BX Ch{} Eta3 Strips[100,150]".format(ch_num))
-        fig_BX_eta3_strips100_150.text(0.8, 0.25*((3-ch_num)+1)-0.05, "Sigma: {}".format(round(norm.fit(x)[1],3)), fontsize=24)
-
-
         x_time_res = np.empty([n_strip_bins*len(eta_list)])
         y_time_res = np.empty([n_strip_bins*len(eta_list)])
         y_time_res_err = np.empty([n_strip_bins*len(eta_list)])
@@ -104,19 +99,6 @@ for bx, name in BX_options:
             axs_occupancy[eta_num-1, ch_num].set_ylabel("Digi Strip")
             axs_occupancy[eta_num-1, ch_num].set_title("Cluster DigiStrip vs {} Ch{} Eta{}".format(name, ch_num, eta_num))
 
-
-            """
-            one_cluster_per_chamber_mask = (ak.sum(events.clusterChamber == ch_num, axis=1) == 1)
-            x = ak.to_numpy(ak.flatten(bx[ch_eta_mask & one_cluster_per_chamber_mask]))
-            y = ak.to_numpy(ak.flatten(strips[ch_eta_mask & one_cluster_per_chamber_mask]))
-
-            tmp_occupancy = axs_occupancy_one_per_chamber[eta_num-1, ch_num].hist2d(x, y, bins=(10,400), range=((0,10),(0,400)), norm='log')
-            fig_occupancy_one_per_chamber.colorbar(tmp_occupancy[3], label="Number of Entries", ax=axs_occupancy_one_per_chamber[eta_num-1, ch_num])
-            axs_occupancy_one_per_chamber[eta_num-1, ch_num].set_xlabel(name)
-            axs_occupancy_one_per_chamber[eta_num-1, ch_num].set_ylabel("Digi Strip")
-            axs_occupancy_one_per_chamber[eta_num-1, ch_num].set_title("Cluster DigiStrip vs {} Masked Ch{} Eta{}".format(name, ch_num, eta_num))
-            """
-
             #The ideal BX for each eta over strip bins
             for strip_bin_num in range(n_strip_bins):
                 strip_start = (strip_bin_num)*(128*3)/n_strip_bins
@@ -130,7 +112,7 @@ for bx, name in BX_options:
                 y = ak.to_numpy(ak.flatten(strips[ch_eta_mask]))
 
                 #Only look at BX less than 7 and strip within bins in the flat numpy
-                mask = (x < 7) & (y >= strip_start) & (y <= strip_end)
+                mask = (x < 7) & (x > 1) & (y >= strip_start) & (y <= strip_end)
                 x1 = x[mask]
                 y1 = y[mask]
 
@@ -141,8 +123,22 @@ for bx, name in BX_options:
                 y_time_res[strip_bin_num + (eta_num-1)*n_strip_bins] = mu
                 y_time_res_err[strip_bin_num + (eta_num-1)*n_strip_bins] = sigma
 
+
+            y_time_res_err_eta = y_time_res_err[(eta_num-1)*n_strip_bins:(eta_num)*n_strip_bins]
+            tmp_time_res_hist_by_eta = axs_timeres_hist_by_eta[eta_num-1, ch_num].hist(y_time_res_err_eta, bins=50, range=(0.2,1.0))
+            axs_timeres_hist_by_eta[eta_num-1, ch_num].set_xlabel("BX Sigma")
+            axs_timeres_hist_by_eta[eta_num-1, ch_num].set_ylabel("Entries")
+            axs_timeres_hist_by_eta[eta_num-1, ch_num].set_title("Time Resolution Ch{} Eta{}".format(ch_num, eta_num))
+            y_time_res_err_eta_nonan = y_time_res_err_eta[~np.isnan(y_time_res_err_eta)]
+            y_time_res_err_eta_nonan = y_time_res_err_eta_nonan[~(y_time_res_err_eta_nonan < 0.1)]
+            fig_mean_by_eta = round(norm.fit(y_time_res_err_eta_nonan)[0],3)
+            axs_timeres_hist_by_eta[eta_num-1, ch_num].text(0.5, 0.8, "Mean: {}".format(fig_mean_by_eta), fontsize=18, transform=axs_timeres_hist_by_eta[eta_num-1, ch_num].transAxes)
+
+
+
+
         tmp_time_res = axs_timeres[ch_num].errorbar(x_time_res, y_time_res, yerr=y_time_res_err)
-        axs_timeres[ch_num].set_xlabel("Strip Bin (x4)")
+        axs_timeres[ch_num].set_xlabel("Strip Bin (x{})".format(n_stips_per_bin))
         axs_timeres[ch_num].set_ylabel("Mean "+name)
         axs_timeres[ch_num].set_title("Time Resolution Ch{}".format(ch_num, eta_num))
         axs_timeres[ch_num].set_ylim(1,5)
@@ -163,9 +159,6 @@ for bx, name in BX_options:
     fig_occupancy.savefig(plot_prefix+"occupancy_"+name+".pdf".format(ch_num, eta_num))
     plt.close(fig_occupancy)
 
-    #fig_occupancy_one_per_chamber.tight_layout()
-    #fig_occupancy_one_per_chamber.savefig(plot_prefix+"occupancy_masked_"+name+".pdf".format(ch_num, eta_num))
-
     fig_timeres.tight_layout()
     fig_timeres.savefig(plot_prefix+"time_res_"+name+".pdf")
     plt.close(fig_timeres)
@@ -174,6 +167,6 @@ for bx, name in BX_options:
     fig_timeres_hist.savefig(plot_prefix+"time_res_hist_"+name+".pdf")
     plt.close(fig_timeres_hist)
 
-    fig_BX_eta3_strips100_150.tight_layout()
-    fig_BX_eta3_strips100_150.savefig(plot_prefix+"BX_eta3_strip100_150_"+name+".pdf")
-    plt.close(fig_BX_eta3_strips100_150)
+    fig_timeres_hist_by_eta.tight_layout()
+    fig_timeres_hist_by_eta.savefig(plot_prefix+"time_res_hist_by_eta_"+name+".pdf")
+    plt.close(fig_timeres_hist_by_eta)
