dbe6fa960cf240df7cd0512fa229b9ff6b359a1a
aebid <aebid@tamu.edu>
2023-12-04 10:54:04 +0100

    Updated variable names to fit new data format

--- digi_analysis/testing_cluster.py
+++ digi_analysis/testing_cluster.py
@@ -8,11 +8,10 @@ from scipy.stats import norm
 import ROOT
 from array import array
 
-plot_prefix = "plots/run154/"
-#plot_prefix = "plots/run146/"
+plot_prefix = "plots/run386/"
 
-fname = "../input_files/digi/00000154.root"
-#fname = "../input_files/digi/00000146.root"
+fname = "../me0_multibx_digi_run386.root"
+outname = "../awk_cluster_run386.root"
 
 #Data looks like
 #orbitNumber     = 15754
@@ -31,14 +30,38 @@ fname = "../input_files/digi/00000154.root"
 #digiStrip       = (vector<int>*)0x600003041ae0
 
 
+
+#New data format 28Nov23
+#orbitNumber     = 45905
+#bunchCounter    = 1820
+#eventCounter    = 32
+#runParameter    = 455
+#pulse_stretch   = 0
+#rawSlot         = (vector<int>*)0x600001914920
+#rawOH           = (vector<int>*)0x600001914940
+#rawVFAT         = (vector<int>*)0x600001914e20
+#rawChannel      = (vector<int>*)0x600001914c20
+#digiBX          = (vector<int>*)0x600001915120
+#digiStripChamber = (vector<int>*)0x600001914980
+#digiStripEta    = (vector<int>*)0x600001915160
+#digiStrip       = (vector<int>*)0x600001914900
+#digiStripCharge = (vector<int>*)0x6000019148e0
+#digiStripTime   = (vector<int>*)0x6000019148c0
+#digiPadChamber  = (vector<int>*)0x6000019148a0
+#digiPadX        = (vector<int>*)0x600001914880
+#digiPadY        = (vector<int>*)0x600001914860
+#digiPadCharge   = (vector<int>*)0x600001914840
+#digiPadTime     = (vector<int>*)0x600001914820
+
+
 uproot_file = uproot.open(fname)
 tree = uproot_file['outputtree']
 events = tree.arrays()
 
 
-out_file = uproot.recreate("cluster_tree.root")
+out_file = uproot.recreate(outname)
 
-
+"""
 class cluster():
     def __init__(self, strips, BXs, chamber, eta, evt_num):
         self.strips = strips
@@ -51,12 +74,15 @@ class cluster():
         self.strip = int(np.average(strips))
         self.BX = np.average(BXs)
         self.evt_num = evt_num
+"""
 
 #Goal is to cluster the digis within a BX window
 
 ch_list = [0,1,2,3]
 eta_list = [1,2,3,4]
 BX_window = [1,7]
+
+fill_none_value = 9999
 
 nEvents = len(events)
 print("There are ", nEvents, " events")
@@ -70,10 +96,10 @@ print("There are ", nEvents, " events")
 #sort_values = [13001, 13002, 13003, 32003, 32004, 32005, 32006, 31002, 31003, 31004, 31005]
 #We do not care about the BX order!!!
 
-BX_mask = (events.digiBX >= BX_window[0]) & (events.digiBX <= BX_window[1])
+BX_mask = (events.digiBX > BX_window[0]) & (events.digiBX < BX_window[1])
 
-digiChamber = events.digiChamber[BX_mask]
-digiEta = events.digiEta[BX_mask]
+digiChamber = events.digiStripChamber[BX_mask]
+digiEta = events.digiStripEta[BX_mask]
 digiBX = events.digiBX[BX_mask]
 digiStrip = events.digiStrip[BX_mask]
 
@@ -105,6 +131,7 @@ new_cluster_mask = (r2 == l2+1) & (digiChamber[ar2] == digiChamber[al2]) & (digi
 
 shape_list = []
 
+#Finds the cuts of the cluster objects, but I couldn't do it in a smart way so I had to use a for loop
 for count, i in enumerate(new_cluster_mask):
     if count%10000 == 1: print("At count ", count)
     value = 1
@@ -123,19 +150,19 @@ clusterChambers = ak.unflatten(chambers, shape_list, axis=1)
 clusterEtas = ak.unflatten(etas, shape_list, axis=1)
 
 #Remove nones
-clusterStrips = ak.fill_none(clusterStrips[~ak.is_none(clusterStrips, axis=2)], 0.0)
-clusterBXs = ak.fill_none(clusterBXs[~ak.is_none(clusterBXs, axis=2)], 0.0)
-clusterChambers = ak.fill_none(clusterChambers[~ak.is_none(clusterChambers, axis=2)], 0.0)
-clusterEtas = ak.fill_none(clusterEtas[~ak.is_none(clusterEtas, axis=2)], 0.0)
+clusterStrips = ak.fill_none(clusterStrips[~ak.is_none(clusterStrips, axis=2)], fill_none_value)
+clusterBXs = ak.fill_none(clusterBXs[~ak.is_none(clusterBXs, axis=2)], fill_none_value)
+clusterChambers = ak.fill_none(clusterChambers[~ak.is_none(clusterChambers, axis=2)], fill_none_value)
+clusterEtas = ak.fill_none(clusterEtas[~ak.is_none(clusterEtas, axis=2)], fill_none_value)
 
 
 
-clusterStrip_min = ak.fill_none(ak.min(clusterStrips, axis=2), 0.0)
-clusterStrip_avg = ak.fill_none(ak.mean(clusterStrips, axis=2), 0.0)
-clusterStrip_size = ak.fill_none(ak.num(clusterStrips, axis=2), 0.0)
+clusterStrip_min = ak.fill_none(ak.min(clusterStrips, axis=2), fill_none_value)
+clusterStrip_avg = ak.fill_none(ak.mean(clusterStrips, axis=2), fill_none_value)
+clusterStrip_size = ak.fill_none(ak.num(clusterStrips, axis=2), fill_none_value)
 
-clusterBX_min = ak.fill_none(ak.min(clusterBXs, axis=2), 0.0)
-clusterBX_max = ak.fill_none(ak.max(clusterBXs, axis=2), 0.0)
+clusterBX_min = ak.fill_none(ak.min(clusterBXs, axis=2), fill_none_value)
+clusterBX_max = ak.fill_none(ak.max(clusterBXs, axis=2), fill_none_value)
 
 tmp_seconds = ak.pad_none(ak.sort(clusterBXs, axis=2), 2, axis=2)
 clusterBX_second = ak.where(
@@ -143,12 +170,12 @@ clusterBX_second = ak.where(
             tmp_seconds[:,:,0],
             tmp_seconds[:,:,1]
     )
-clusterBX_second = ak.fill_none(clusterBX_second, 0.0)
+clusterBX_second = ak.fill_none(clusterBX_second, fill_none_value)
 #clusterBX_second = ak.pad_none(ak.sort(clusterBXs, axis=2), 2, axis=2)[:,:,1]
-clusterBX_avg = ak.fill_none(ak.mean(clusterBXs, axis=2), 0.0)
+clusterBX_avg = ak.fill_none(ak.mean(clusterBXs, axis=2), fill_none_value)
 
-clusterChamber = ak.fill_none(ak.mean(clusterChambers, axis=2), 0.0)
-clusterEta = ak.fill_none(ak.mean(clusterEtas, axis=2), 0.0)
+clusterChamber = ak.fill_none(ak.mean(clusterChambers, axis=2), fill_none_value)
+clusterEta = ak.fill_none(ak.mean(clusterEtas, axis=2), fill_none_value)
 
 
 out_file["clusterTree"] = {
